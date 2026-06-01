8ee2633faf38b2aeaef9739107af6a5d9b1c6b64
smRid <omorsk911@gmail.com>
2026-06-01 16:07:04 +0600

    implement automated user creation provider using axios

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+'use client'
+
+import axios from 'axios';
+import React, { useEffect } from 'react'
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+useEffect(() => {
+  CreateNewUser();
+}, [])
+
+const CreateNewUser = async () => {
+  const result = await axios.post('/api/users', {});
+
+  console.log("Result", result);
+}
+
   return (
     <div>{children}</div>
   )
