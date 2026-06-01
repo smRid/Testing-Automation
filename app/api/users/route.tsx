7b5bdf05e0fe9e6e48b62ddd4bc5f0cc0bb60a9a
smRid <omorsk911@gmail.com>
2026-06-01 16:46:14 +0600

    update user synchronization service and database registration

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -4,33 +4,30 @@ import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  try{
-    const user=await currentUser();
+  try {
+    const user = await currentUser();
     const email = user?.primaryEmailAddress?.emailAddress;
 
     if (!user || !email) {
-      return NextResponse.json({error:"Unauthorized"},{status:401})
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const userResult=await db.select().from(users).where(
-      eq(users.email,email)
+    const userResult = await db.select().from(users).where(
+      eq(users.email, email)
     );
 
     if (userResult.length == 0) {
-    const newUser = await db.insert(users).values({
+      const newUser = await db.insert(users).values({
         email,
         name: user.fullName ?? 'New User'
-    }).returning()
+      }).returning();
 
-    return NextResponse.json({user:newUser[0]})
+      return NextResponse.json({ user: newUser[0] });
+    } else {
+      return NextResponse.json({ user: userResult[0] });
     }
-    else {
-        return NextResponse.json({ user: userResult[0] })
-    }
-    
-  }
-  catch (e) {
-    console.log("Error Creating User: ",e)
-    return NextResponse.json({error:"Failed to create new user"},{status:500})
+  } catch (e) {
+    console.log("Error Creating User: ", e);
+    return NextResponse.json({ error: "Failed to create new user" }, { status: 500 });
   }
 }
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,35 +1,35 @@
-'use client'
+'use client';
 
 import { useUser } from '@clerk/nextjs';
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-const { isLoaded, isSignedIn } = useUser();
+  const { isLoaded, isSignedIn } = useUser();
 
-useEffect(() => {
-  if (!isLoaded || !isSignedIn) {
-    return;
-  }
+  useEffect(() => {
+    if (!isLoaded || !isSignedIn) {
+      return;
+    }
 
-  CreateNewUser().catch((error) => {
-    console.log("Error syncing user: ", error);
-  });
-}, [isLoaded, isSignedIn])
+    const createNewUser = async () => {
+      const result = await axios.post('/api/users', {});
 
-const CreateNewUser = async () => {
-  const result = await axios.post('/api/users', {});
+      console.log("Result", result);
+    };
 
-  console.log("Result", result);
-}
+    createNewUser().catch((error) => {
+      console.log("Error syncing user: ", error);
+    });
+  }, [isLoaded, isSignedIn]);
 
   return (
     <div>{children}</div>
-  )
+  );
 }
 
-export default Provider
+export default Provider;
