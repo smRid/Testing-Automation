cc019772565e0e0248913bfbf185bf19982310ba
smRid <omorsk911@gmail.com>
2026-06-02 16:43:34 +0600

    feat: add WorkspaceBody component with GitHub repository connection UI

--- components/custom/WorkspaceBody.tsx
+++ components/custom/WorkspaceBody.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { UserDetailContext } from '@/context/UserDetailContext'
 import React, { useContext } from 'react'
+import { Card } from '../ui/card';
+import Image from 'next/image';
+import { Button } from '../ui/button';
 
 function WorkspaceBody() {
 
@@ -12,6 +15,16 @@ function WorkspaceBody() {
             <h2 className='text-4xl font-medium'>Workspace</h2>
             <h2 className='bg-purple-100 p-2 rounded-lg font-medium text-purple-700'>{userDetail?.credits } Credits remains:</h2>
         </div>
+
+        <Card className={'mt-5 flex justify-between items-center'}>
+            <div className='flex items-center gap-5'>
+                <Image src={'/github.svg'} alt='github' width={50} height={50} />
+                <h2 className='text-lg'>Connect Github & Add Repository</h2>
+            </div>
+            <div>
+                <Button className='cursor-pointer'>Install</Button>
+            </div>
+        </Card>
     </div>
   )
 }
