5a43ef3cf3f50a4190b7eb106eb7057630504b79
smRid <omorsk911@gmail.com>
2026-06-03 14:09:37 +0600

    create API route to fetch all GitHub repositories using token from cookies

---
+++ app/api/github/repos/route.tsx
@@ -0,0 +1,29 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const cookiesStore = await cookies();
+  const token = cookiesStore.get('gh_token')?.value;
+
+  if (!token) {
+    return NextResponse.json(JSON.stringify({ error: 'Github token not found' }), { status: 401 });
+  }
+
+  const allRespo = [];
+  let page = 1;
+
+  while (true) {
+    const res = await fetch(`https://api.github.com/user/repos?per_page=100&page=${page}&sort=updated`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Acceot: 'application/vnd.github+json'
+      }
+    }
+    )
+    const respos=await res.json();
+    if(!respos.length) break;
+    allRespo.push(...respos);
+    page++;
+  }
+  return NextResponse.json({ repos: allRespo }, { status: 200 });
+}
